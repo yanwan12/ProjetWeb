ab9294ca6d41fb8ff7a8c189a67cd9419746771f
yanwan12 <32262334+yanwan12@users.noreply.github.com>
2017-12-21 13:47:31 +0100

    modifs php et base
    création base + avancement php

--- ProjetPHP/traitement.php
+++ ProjetPHP/traitement.php
@@ -2,32 +2,51 @@
 <html>
 
 <head>
-    <h1>Vous êtes connectés</h1>
+    <h1>Vous êtes connecté</h1>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
 </head>
-<div>
+<body>
+        <div>
+                <?php
+                try
+                {
+                    // On se connecte à MySQL
+                    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+                }
+                catch(Exception $e)
+                {
+                    // En cas d'erreur, on affiche un message et on arrête tout
+                        die('Erreur : '.$e->getMessage());
+                }
+
+                if (isset($_POST['nom']) AND isset($_POST['mail']))
+                    {
+                    $nom=$_POST['nom'];
+                    $mail=$_POST['mail'];
+                echo('<body>
+                        <table>
+                            <tr>
+                                <td>Pseudo :</td>
+                                <td>'.$nom.'</td>
+                                
+                            </tr>
+                            <tr>
+                                <td>Mail :</td>
+                                <td>'.$mail.'</td>
+                            </tr> 
+                        </table></body>');
+                    
+                
+                        echo date('D/M/Y h:i:s');
+                    }
+                ?>
+        </div>
+</body>
 <?php
-if (isset($_POST['nom']) AND isset($_POST['mail']))
-    {
-    $nom=$_POST['nom'];
-    $mail=$_POST['mail'];
-   echo('<body>
-           <table>
-               <tr>
-                   <td>Pseudo :</td>
-                   <td>'.$nom.'</td>
-                   
-               </tr>
-               <tr>
-                   <td>Mail :</td>
-                   <td>'.$mail.'</td>
-               </tr> 
-           </table></body>');
-      
-   
-           echo date('D/M/Y h:i:s');
-    }
-   ?>
-</div>
+}
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
 </html>
