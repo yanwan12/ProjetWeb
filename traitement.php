cd380848f8c59a6462dc998db2428bb5425e5a09
yanwan12 <32262334+yanwan12@users.noreply.github.com>
2018-01-26 10:01:51 +0100

    résolution bug insrt
    fix du bug d'insertion bdd

--- traitement.php
+++ traitement.php
@@ -2,51 +2,54 @@
 <html>
 
 <head>
-    <h1>Vous êtes connecté</h1>
-    <link rel="stylesheet" href="style.css">
-    <meta charset="UTF-8">
-</head>
-<body>
         <div>
                 <?php
                 try
                 {
                     // On se connecte à MySQL
-                    $bdd = new PDO('mysql:host=localhost;dbname=base projet;charset=utf8', 'root', '');
-                }
-                catch(Exception $e)
-                {
-                    // En cas d'erreur, on affiche un message et on arrête tout
-                        die('Erreur : '.$e->getMessage());
-                }
-                
+                    $bdd = new PDO('mysql:host=127.0.0.1;dbname=base_projet;charset=utf8', 'ecom', 'ecom');
+                    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 if (isset($_POST['pseudo']) AND isset($_POST['nom']) AND isset($_POST['pnom']) AND isset($_POST['age']) AND isset($_POST['mail']))
                     {
-                    $pseudo=$_POST['pseudo'];    
-                    $nom=$_POST['nom'];
-                    $pnom=$_POST['pnom'];
-                    $age=$_POST['age'];
-                    $mail=$_POST['mail'];
-                    $datte= date('d/M/Y h:i');
                         
-                    try
-                {
-                     $req=$bdd->prepare('INSERT INTO user (pseudo,nom,prenom,age,mail,datte) VALUES (:pseudo, :nom, :pnom, :age, :mel, CURRENT_TIMESTAMP())');
-                     $req->execute(array(
-                        'pseudo'=>$pseudo,
-                        'nom' => $nom,
-                        'pnom' => $pnom,
-                        'age' => $age,
-                        'mel' => $mail));
+                    $pseudo=$_POST['pseudo'];    
+                    $nom = $_POST['nom'];
+                    $pnom = $_POST['pnom'];
+                    $age = $_POST['age'];
+                    $mel = $_POST['mail'];
+                    $datte = date('d/M/Y h:i');
+                        
+                            $req = $bdd->prepare('INSERT INTO user (pseudo,nom,prenom,age,mail) VALUES(:pseudo, :nom, :pnom, :age, :mel)');
+                            
+                            $req->execute(array(
+                                    ':pseudo'=>$pseudo,
+                                    ':nom' => $nom,
+                                    ':pnom' => $pnom,
+                                    ':age' => $age,
+                                    ':mel' => $mel
+                                    )
+                            );
+                      
+
+                     
+                       
+                    } else {
+                        echo "ça marche pas";
+                    }
                 }
+               
                 catch(Exception $e)
                 {
                     // En cas d'erreur, on affiche un message et on arrête tout
-                        die('Erreur : '.$e->getMessage());
+                    die('Erreur : '.$e->getMessage());
                 }
-                       
-                    }
                 ?>
         </div>
+        <h1>Bienvenu <?php  ?>
+        </h1>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+</head>
+<body>        
 </body>
 </html>
